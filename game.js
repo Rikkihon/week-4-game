935bd8c0049c002481605d7dcba09b1baa70186d
Rikki Honnold <rikki@er-10-132-80-218.wireless.umn.edu>
2018-03-10 10:26:52 -0600

    uploading more files

--- game.js
+++ game.js
@@ -10,36 +10,36 @@ $( "YourScore2" )
 
 //wins declared here
 
-if (yourScore === starting) {
-    wins = true;
-    wins ++;
-    alert("You win!");
-    text(wins);
-}
+// if (yourScore === starting) {
+//     wins = true;
+//     wins ++;
+//     alert("You win!");
+//     text(wins);
+// }
 
-else if (yourscore > starting) {
-   losses = true;
-   losses ++;
-   alert("You lose!!");
-   text(losses);
-}
+// else if (yourscore > starting) {
+//    losses = true;
+//    losses ++;
+//    alert("You lose!!");
+//    text(losses);
+// }
 
 
-$( "#crystal1" ).click(function() {
+// $( "#crystal1" ).click(function() {
     
-    alert("New score: " + counter);
-  });
-  $( "#crystal2" ).click(function() {
+//     alert("New score: " + counter);
+//   });
+//   $( "#crystal2" ).click(function() {
  
-    alert("New score: " + counter);
-  });
-  $( "#crystal3" ).click(function() {
+//     alert("New score: " + counter);
+//   });
+//   $( "#crystal3" ).click(function() {
    
-    alert("New score: " + counter);
-  });
-  $( "#crystal4" ).click(function() {
-    alert("New score: " + counter);
-  });
+//     alert("New score: " + counter);
+//   });
+//   $( "#crystal4" ).click(function() {
+//     alert("New score: " + counter);
+//   });
 
 // The random number shown at the start of the game should be between 19 - 120.
 // Each crystal should have a random hidden value between 1 - 12.
